bfb9f8deac919a6cc76717f84016e7c9cf9cb166
M977986310 <977986310@qq.com>
2017-05-21 13:34:10 +0800

    five

--- main/main.js
+++ main/main.js
@@ -1,18 +1,37 @@
 module.exports = function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
     console.log(`o) 最小值 = ${sequence.minimum()}
-// Write your code here
-`);
+o) 最大值 = ${sequence.maximum()}
+o) 元素数量 = ${sequence.elementNumber()}
+o) 平均值 = ${sequence.average()}`);
 };
 
 class Sequence {
-  constructor(input) {
-    // Write your code here
-  }
+    constructor(input) {
 
-  minimum() {
-    // Write your code here
-  }
+        (this.input = input).sort((a, b) => a - b);
+    }
 
-  // Write your code here
+    minimum() {
+
+        return this.input[0];
+    }
+
+    maximum() {
+
+        return this.input[this.input.length - 1];
+    }
+
+    elementNumber(){
+
+        return this.input.length;
+    }
+
+    average(){
+
+        return (this.input.reduce((a, b) => (a + b))/this.input.length).toFixed(2);
+    }
+
+
+    // Write your code here
 }
